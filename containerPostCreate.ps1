cd4ec6dd0738f4d99d8716f40ce64db656c549ac
Johan Ljunggren <johan.ljunggren@shamn.se>
2024-09-18 11:29:11 +0200

    Fix post script

--- containerPostCreate.ps1
+++ containerPostCreate.ps1
@@ -5,7 +5,7 @@ $PSDefaultParameterValues['Install-ModuleFast:Timeout'] = 180
 
 & ([scriptblock]::Create((Invoke-WebRequest -Uri 'bit.ly/modulefast')))
 
-Install-ModuleFast -Path './DscCommunity.MaintenanceAutomation/requirements.psd1' -Specification 'Pester' -Verbose
-Install-ModuleFast -Path './devRequirements.psd1' -Specification 'Pester' -Verbose
+Install-ModuleFast -Path './DscCommunity.MaintenanceAutomation/requirements.psd1' -Verbose
+Install-ModuleFast -Path './devRequirements.psd1' -Verbose
 
 func extensions install
