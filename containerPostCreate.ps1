8713c91204557a854a7b5686a3f3c404059b2df6
Johan Ljunggren <johan.ljunggren@shamn.se>
2024-09-18 11:31:53 +0200

    Fix post script

--- containerPostCreate.ps1
+++ containerPostCreate.ps1
@@ -3,9 +3,18 @@ $PSDefaultParameterValues['Install-ModuleFast:NoPSModulePathUpdate'] = $true
 $PSDefaultParameterValues['Install-ModuleFast:NoProfileUpdate'] = $true
 $PSDefaultParameterValues['Install-ModuleFast:Timeout'] = 180
 
+Write-Information -MessageData "Installing ModuleFast" -InformationAction Continue
+
 & ([scriptblock]::Create((Invoke-WebRequest -Uri 'bit.ly/modulefast')))
 
+Write-Information -MessageData "Installing runtime dependencies" -InformationAction Continue
+
 Install-ModuleFast -Path './DscCommunity.MaintenanceAutomation/requirements.psd1' -Verbose
+
+Write-Information -MessageData "Installing dev dependencies" -InformationAction Continue
+
 Install-ModuleFast -Path './devRequirements.psd1' -Verbose
+
+Write-Information -MessageData "Installing Azure Functions extensions" -InformationAction Continue
 
 func extensions install
